01a0be51ac3a8a0a7d604b685150949637fcb7f7
Gustavo Oliveira <gustavo.ramosue@gmail.com>
2023-03-05 23:19:39 -0300

    Add project files.

---
+++ CalculoFibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int digit_u =0, ant = 1, prox = 0, atual = 1;
+
+        Console.WriteLine("Informe o número para o cálculo ");
+        digit_u = int.Parse(Console.ReadLine());
+        Console.Write($"A sequencia fibonacci de {digit_u} é: ");
+        for (int count = 0; count < digit_u; count++) {
+            Console.Write(ant + " ");
+            prox = atual + ant;
+            ant = atual;
+            atual = prox;
+        }
+    }
+}
